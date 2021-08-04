9b5acd0aa011ad324e19f00bd470959fa6134be9
yeahwu <yeahwu@users.noreply.github.com>
2021-08-05 01:09:44 +0800

    Update tcp-wss.sh

--- tcp-wss.sh
+++ tcp-wss.sh
@@ -13,11 +13,11 @@ v2uuid=$(cat /proc/sys/kernel/random/uuid)
 
 install_ssl(){
     systemctl stop nginx.service
-    isport=`netstat -ntlp| grep -E ':80 |:443 '`
-    if [[ "${isport}" != "" ]]; then
-            echo " 80或443端口被占用请先释放端口，再运行脚本"
+    isPort=`netstat -ntlp| grep -E ':80 |:443 '`
+    if [ "$isPort" != "" ];then
+            echo " 80或443端口被占用，请先释放端口再运行此脚本"
             echo " 端口占用信息如下："
-            echo ${isport}
+            echo $isPort
             exit 1
     fi
     
