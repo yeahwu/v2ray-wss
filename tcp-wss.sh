93f25b47297cc8a3e595e6eff6260f42dabae797
yeahwu <yeahwu@users.noreply.github.com>
2021-08-04 12:13:49 +0800

    Update tcp-wss.sh

--- tcp-wss.sh
+++ tcp-wss.sh
@@ -92,7 +92,7 @@ PIDFile=/var/run/nginx.pid
 ExecStartPre=/usr/sbin/nginx -t
 ExecStart=/usr/sbin/nginx
 ExecReload=/usr/sbin/nginx -s reload
-ExecStop=/bin/kill -s QUIT $MAINPID
+ExecStop=/bin/kill -s QUIT \$MAINPID
 PrivateTmp=true
 [Install]
 WantedBy=multi-user.target
