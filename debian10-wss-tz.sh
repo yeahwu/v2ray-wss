5dac0802fd79939fa1835da0586857b67a0098b3
yeahwu <yeahwu@users.noreply.github.com>
2021-08-26 01:02:45 +0800

    Update debian10-wss-tz.sh

--- debian10-wss-tz.sh
+++ debian10-wss-tz.sh
@@ -12,11 +12,10 @@ timedatectl set-timezone Asia/Shanghai
 echo "====输入已经DNS解析好的域名===="
 read domain
 
-apt install -y nginx certbot php7.3 php7.3-fpm
-
-systemctl stop nginx.service
+apt install -y certbot php7.3 php7.3-fpm build-essential libtool libpcre3 libpcre3-dev zlib1g-dev openssl libssl-dev
 
 echo "A" | certbot certonly --renew-by-default --register-unsafely-without-email --standalone -d $domain
+echo -e "0 2 1 * * /usr/bin/certbot renew --pre-hook \"service nginx stop\" --post-hook \"service nginx start\"" >> /var/spool/cron/crontabs/root
 
 sed -ri 's|listen = /run/php/php7.3-fpm.sock|listen = 127.0.0.1:9000|' /etc/php/7.3/fpm/pool.d/www.conf
 
@@ -27,6 +26,46 @@ mkdir -p /var/www/html
 wget https://111111.online/cloud/docs/tz.php.txt -O /var/www/html/tz.php
 
 v2path=$(cat /dev/urandom | head -1 | md5sum | head -c 6)
+
+
+    wget https://nginx.org/download/nginx-1.21.1.tar.gz -O - | tar -xz
+    cd nginx-1.21.1
+    ./configure --prefix=/etc/nginx \
+    --sbin-path=/usr/sbin/nginx \
+    --modules-path=/usr/lib/nginx/modules \
+    --conf-path=/etc/nginx/nginx.conf \
+    --error-log-path=/var/log/nginx/error.log \
+    --http-log-path=/var/log/nginx/access.log \
+    --pid-path=/var/run/nginx.pid \
+    --lock-path=/var/run/nginx.lock \
+    --with-http_v2_module \
+    --with-http_ssl_module \
+    --with-http_gzip_static_module \
+    --with-http_stub_status_module \
+    --with-http_sub_module \
+    --with-stream \
+    --with-stream_ssl_module
+    
+    make && make install
+    cd ..
+    rm -rf nginx-1.21.1
+    
+cat >/lib/systemd/system/nginx.service<<EOF
+[Unit]
+Description=The NGINX HTTP and reverse proxy server
+After=syslog.target network-online.target remote-fs.target nss-lookup.target
+Wants=network-online.target
+[Service]
+Type=forking
+PIDFile=/var/run/nginx.pid
+ExecStartPre=/usr/sbin/nginx -t
+ExecStart=/usr/sbin/nginx
+ExecReload=/usr/sbin/nginx -s reload
+ExecStop=/bin/kill -s QUIT \$MAINPID
+PrivateTmp=true
+[Install]
+WantedBy=multi-user.target
+EOF
 
 cat >/etc/nginx/nginx.conf<<EOF
 pid /var/run/nginx.pid;
@@ -69,7 +108,7 @@ http {
         listen [::]:443 ssl http2;
         server_name $domain;
         root /var/www/html;
-        ssl_protocols TLSv1 TLSv1.1 TLSv1.2 TLSv1.3;
+        ssl_protocols TLSv1.1 TLSv1.2 TLSv1.3;
         ssl_ciphers ECDHE-RSA-AES128-GCM-SHA256:HIGH:!aNULL:!MD5:!RC4:!DHE;
         ssl_prefer_server_ciphers on;
         ssl_certificate /etc/letsencrypt/live/$domain/fullchain.pem;
