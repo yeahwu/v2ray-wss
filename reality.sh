3b011164a16bfa4d634f750c9eeb73a8936082c9
yeahwu <yeahwu@users.noreply.github.com>
2023-05-02 00:56:07 +0800

    Create reality.sh

---
+++ reality.sh
@@ -0,0 +1,125 @@
+#!/bin/sh
+# forum: https://1024.day
+
+if [[ $EUID -ne 0 ]]; then
+    clear
+    echo "Error: This script must be run as root!" 1>&2
+    exit 1
+fi
+
+timedatectl set-timezone Asia/Shanghai
+v2uuid=$(cat /proc/sys/kernel/random/uuid)
+ipaddr=$(hostname -I)
+
+install_xray(){ 
+    if [ -f "/usr/bin/apt-get" ]; then
+        apt-get update -y
+        apt-get install -y gawk curl
+    else
+        yum update -y
+        yum install -y epel-release
+        yum install -y gawk curl
+    fi
+    bash -c "$(curl -L https://github.com/XTLS/Xray-install/raw/main/install-release.sh)" @ install --version 1.8.1
+}
+
+reconfig(){
+    reX25519Key=$(/usr/local/bin/xray x25519)
+    rePrivateKey=$(echo "${reX25519Key}" | head -1 | awk '{print $3}')
+    rePublicKey=$(echo "${reX25519Key}" | tail -n 1 | awk '{print $3}')
+
+cat >/usr/local/etc/xray/config.json<<EOF
+{
+    "inbounds": [
+        {
+            "port": 443,
+            "protocol": "vless",
+            "settings": {
+                "clients": [
+                    {
+                        "id": "$v2uuid",
+                        "flow": "xtls-rprx-vision"
+                    }
+                ],
+                "decryption": "none"
+            },
+            "streamSettings": {
+                "network": "tcp",
+                "security": "reality",
+                "realitySettings": {
+                    "show": false,
+                    "dest": "www.amazon.com:443",
+                    "xver": 0,
+                    "serverNames": [
+                        "amazon.com",
+                        "www.amazon.com"
+                    ],
+                    "privateKey": "$rePrivateKey",
+                    "minClientVer": "",
+                    "maxClientVer": "",
+                    "maxTimeDiff": 0,
+                    "shortIds": [
+                        "88",
+                        "888888"
+                    ]
+                }
+            }
+        }
+    ],
+    "outbounds": [
+        {
+            "protocol": "freedom",
+            "tag": "direct"
+        },
+        {
+            "protocol": "blackhole",
+            "tag": "blocked"
+        }
+    ]    
+}
+EOF
+
+    systemctl enable xray.service && systemctl restart xray.service
+    rm -f tcp-wss.sh install-release.sh
+
+cat >/usr/local/etc/xray/reclient.json<<EOF
+{
+===========配置参数=============
+代理模式：vless
+地址：${ipaddr}
+端口：443
+UUID：${v2uuid}
+流控：xtls-rprx-vision
+传输协议：tcp
+Public key：${rePublicKey}
+底层传输：reality
+SNI: www.amazon.com
+shortIds: 88
+}
+EOF
+
+    clear
+}
+
+client_re(){
+    echo
+    echo "安装已经完成"
+    echo
+    echo "===========reality配置参数============"
+    echo "代理模式：vless"
+    echo "地址：${ipaddr}"
+    echo "端口：443"
+    echo "UUID：${v2uuid}"
+    echo "流控：xtls-rprx-vision"
+    echo "传输协议：tcp"
+    echo "Public key：${rePublicKey}"
+    echo "底层传输：reality"
+    echo "SNI: www.amazon.com"
+    echo "shortIds: 88"
+    echo
+}
+
+
+install_xray
+reconfig
+client_re
