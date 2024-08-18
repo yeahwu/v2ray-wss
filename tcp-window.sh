db7fd3cd64ff841d0af908019ec4ebde399d9b6d
yeahwu <yeahwu@users.noreply.github.com>
2024-08-18 23:52:45 +0800

    Create tcp-window.sh
    Available for Debian and Ubuntu systems

---
+++ tcp-window.sh
@@ -0,0 +1,51 @@
+##!/bin/sh
+# Issues https://1024.day
+
+if [[ $EUID -ne 0 ]]; then
+    clear
+    echo "Error: This script must be run as root!"
+    exit 1
+fi
+
+timedatectl set-timezone Asia/Shanghai
+
+cat >/etc/security/limits.conf<<EOF
+* soft     nproc          655360
+* hard     nproc          655360
+* soft     nofile         655360
+* hard     nofile         655360
+
+root soft     nproc          655360
+root hard     nproc          655360
+root soft     nofile         655360
+root hard     nofile         655360
+
+bro soft     nproc          655360
+bro hard     nproc          655360
+bro soft     nofile         655360
+bro hard     nofile         655360
+EOF
+
+echo "session required pam_limits.so" >> /etc/pam.d/common-session
+
+echo "session required pam_limits.so" >> /etc/pam.d/common-session-noninteractive
+
+echo "DefaultLimitNOFILE=655360" >> /etc/systemd/system.conf
+
+cat >/etc/sysctl.conf<<EOF
+fs.file-max = 655360
+net.ipv4.tcp_congestion_control = bbr
+net.core.default_qdisc = fq
+net.ipv4.tcp_slow_start_after_idle = 0
+net.ipv4.tcp_rmem = 8192 262144 536870912
+net.ipv4.tcp_wmem = 4096 16384 536870912
+net.ipv4.udp_rmem_min = 8192
+net.ipv4.udp_wmem_min = 8192
+net.ipv4.tcp_adv_win_scale = -2
+net.ipv4.tcp_notsent_lowat = 131072
+#net.ipv6.conf.all.disable_ipv6 = 1
+#net.ipv6.conf.default.disable_ipv6 = 1
+#net.ipv6.conf.lo.disable_ipv6 = 1
+EOF
+
+sleep 3 && reboot >/dev/null 2>&1
