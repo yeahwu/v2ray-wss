1f0e0a0a668c41fb88109531b18af44498bdce00
yeahwu <yeahwu@users.noreply.github.com>
2025-06-17 18:00:30 +0800

    Update tcp-window.sh

--- tcp-window.sh
+++ tcp-window.sh
@@ -47,6 +47,7 @@ net.ipv4.tcp_notsent_lowat = 131072
 #net.ipv6.conf.all.disable_ipv6 = 1
 #net.ipv6.conf.default.disable_ipv6 = 1
 #net.ipv6.conf.lo.disable_ipv6 = 1
+#net.ipv4.ip_forward = 1
 EOF
 
 rm tcp-window.sh
